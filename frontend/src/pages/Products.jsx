d601e93eba8b3d34434263197e2d12f8344d8f93
nithinvarma <nithinvarma411@gmail.com>
2025-01-23 13:37:17 +0530

    completed assignment-8

---
+++ frontend/src/pages/Products.jsx
@@ -0,0 +1,41 @@
+function Products({ name, image, description, actualPrice, sellingPrice }) {
+  const sizes = ["6", "7", "8", "9", "10", "11", "12"]; // Example sizes, you can modify this list as needed
+
+  return (
+    <div className="bg-gray-300 p-4 rounded-lg flex flex-col justify-between">
+      <div className="w-full">
+        <img
+          src={image}
+          alt={name}
+          className="w-full h-56 object-cover rounded-lg mb-2"
+        />
+        <h2 className="text-lg font-bold">{name}</h2>
+        <p className="text-base opacity-50 my-2">{description}</p>
+      </div>
+      <div className="w-full">
+        <p className="text-lg font-bold my-2">Original Price: ${actualPrice}</p>
+        <p className="text-lg font-bold my-2">Selling Price: ${sellingPrice}</p>
+        <div className="my-2">
+          <label htmlFor="size" className="block text-sm font-semibold mb-1">
+            Select Size:
+          </label>
+          <select
+            id="size"
+            className="w-full p-2 rounded-md border border-gray-400"
+          >
+            {sizes.map((size, index) => (
+              <option key={index} value={size}>
+                {size}
+              </option>
+            ))}
+          </select>
+        </div>
+        <button className="w-full text-white px-4 py-2 rounded-md bg-neutral-900">
+          ADD TO CART
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Products;
